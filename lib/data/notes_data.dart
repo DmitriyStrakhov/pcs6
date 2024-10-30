57d9493887ee6cf958884c9e17c03ec602d220dd
DmitriyStrakhov <Strakhovofficial@yandex.ru>
2024-10-30 18:15:18 +0300

    first commit

---
+++ lib/data/notes_data.dart
@@ -0,0 +1,19 @@
+import '../models/note.dart';
+
+final List<Note> notes = [
+  Note(
+      "Видеокарта MSI GeForce RTX 4080 SUPER GAMING TRIO [GeForce RTX 4080 SUPER 16G GAMING TRIO]",
+      "Видеокарта MSI GeForce RTX 4080 SUPER GAMING TRIO — это мощное графическое решение, основанное на архитектуре NVIDIA Ada Lovelace. Она предназначена для высокопроизводительных игр и задач, связанных с графикой. ",
+      "https://c.dns-shop.ru/thumb/st1/fit/500/500/2eb3adde997b6a1ec3ec792c23aacb34/2185ee82b55a84a3c33ddfd6e7dc95412d6cd257a16a6a8a7c65c8f4c968dddc.jpg.webp",
+      144999.0),
+  Note(
+      "Видеокарта MSI GeForce RTX 4080 SUPER GAMING X SLIM WHITE [912-V511-232]",
+      "Видеокарта MSI GeForce RTX 4080 SUPER GAMING TRIO — это мощное графическое решение, основанное на архитектуре NVIDIA Ada Lovelace. Она предназначена для высокопроизводительных игр и задач, связанных с графикой. В белом цвете.",
+      "https://c.dns-shop.ru/thumb/st1/fit/500/500/812646da5a90b31ad980cb08422fa769/b586795930c85b993010fce63d3e7f9229c282aed8141549aa9894544c34dd89.jpg.webp",
+      144999.0),
+  Note(
+      "Видеокарта MSI GeForce RTX 4070 Ti SUPER GAMING SLIM WHITE [GeForce RTX 4070 Ti SUPER 16G GAMING SLIM WHITE]",
+      "Видеокарта MSI GeForce RTX 4070 Ti SUPER GAMING SLIM — это мощное графическое решение, основанное на архитектуре NVIDIA Ada Lovelace. Она предназначена для высокопроизводительных игр и задач, связанных с графикой.",
+      "https://c.dns-shop.ru/thumb/st1/fit/500/500/1acc0c4700757882fe2e18d6a2baadfd/9d4251b7896e07bbbe11476a38d13d38fbcb64dfba6047ad721f6ef3eb6562a0.jpg.webp",
+      115999.0),
+];
